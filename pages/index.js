2b3195dff27111de27c4d1e08d42f7977d15091e
unknown <diloch111@gmail.com>
2024-01-27 04:33:17 +0000

    create slug and frontmatter

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import matter from "gray-matter";
 import Head from "next/head";
 
 export default function Home({ posts }) {
@@ -16,11 +17,31 @@ export default function Home({ posts }) {
 
 // get the markdown posts
 export async function getStaticProps() {
+  // get markdown files from posts directory
   const files = fs.readdirSync(path.join("posts"));
+
+  // get slug and front matter from posts
+  const posts = files.map((filename) => {
+    //create slug
+    const slug = filename.replace(".md", "");
+
+    // get front matter
+    const markDownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+      const {data:frontmatter} = matter(markDownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
 
   return {
     props: {
-      posts: "The Posts",
+      posts,
     },
   };
 }
